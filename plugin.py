df28dcd5126b05730502f03c6d93480361cbae9b
Vishal Desai <vishaldesai4518@gmail.com>
2026-09-24 22:52:57 +0530

    Add files via upload

---
+++ plugin.py
@@ -0,0 +1,54 @@
+from qgis.PyQt.QtWidgets import QAction, QInputDialog
+from qgis.core import QgsProject, QgsFeature, QgsGeometry
+from qgis.gui import QgsMapToolEmitPoint
+
+class AddTerminalTool(QgsMapToolEmitPoint):
+    def __init__(self, iface, plugin):
+        super().__init__(iface.mapCanvas())
+        self.iface = iface
+        self.plugin = plugin
+
+    def canvasReleaseEvent(self, event):
+        point = self.toMapCoordinates(event.pos())
+        term_layer = self.plugin.term_layer
+        wire_layer = self.plugin.wire_layer
+        terminal_size = 1
+        for wire in wire_layer.getFeatures():
+            if wire.geometry().contains(QgsGeometry.fromPointXY(point)):
+                try:
+                    terminal_size = int(float(str(wire["Size"])))
+                except:
+                    terminal_size = 1
+                break
+        terminal_name = f"T{self.plugin.next_terminal_no}"
+        start_count = self.plugin.current_count
+        end_count = start_count + terminal_size - 1
+        feat = QgsFeature(term_layer.fields())
+        feat.setGeometry(QgsGeometry.fromPointXY(point))
+        feat["TRM_NAME"] = terminal_name
+        feat["TRM_SIZE"] = terminal_size
+        feat["START_COUN"] = start_count
+        feat["TRM_COUNTS"] = f"{start_count}-{end_count}"
+        term_layer.startEditing()
+        term_layer.addFeature(feat)
+        term_layer.commitChanges()
+        self.plugin.current_count = end_count + 1
+        self.plugin.next_terminal_no += 1
+
+class TerminalAutoPlugin:
+    def __init__(self, iface):
+        self.iface = iface
+    def initGui(self):
+        self.action = QAction("Auto Terminal Placement", self.iface.mainWindow())
+        self.action.triggered.connect(self.run)
+        self.iface.addToolBarIcon(self.action)
+    def unload(self):
+        self.iface.removeToolBarIcon(self.action)
+    def run(self):
+        self.term_layer = QgsProject.instance().mapLayersByName("TERMINAL")[0]
+        self.wire_layer = QgsProject.instance().mapLayersByName("Wirelimit")[0]
+        self.current_count, ok = QInputDialog.getInt(None, "Starting Count", "Enter starting count", 1)
+        if not ok: return
+        self.next_terminal_no = self.term_layer.featureCount() + 1
+        self.tool = AddTerminalTool(self.iface, self)
+        self.iface.mapCanvas().setMapTool(self.tool)
